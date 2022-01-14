0be933b54bcd0f7e6982b2246b6c653f29964178
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-14 06:59:52 +0200

    creating a repository

---
+++ lib/data/repository.dart
@@ -0,0 +1,40 @@
+import 'models/models.dart';
+
+// Create a repository interface that will provide methods to add and delete recipes and ingredients.
+abstract class Repository {
+  // Finding recipes and ingredients:
+
+  // Return all recipes in the repository.
+  List<Recipe> findAllRecipes();
+  // Find a specific recipe by its ID.
+  Recipe findRecipeById(int id);
+  // Return all ingredients.
+  List<Ingredient> findAllIngredients();
+  // Find all the ingredients for the given recipe ID.
+  List<Ingredient> findRecipeIngredients(int recipeId);
+
+  // Adding recipes and ingredients:
+
+  // Insert a new recipe.
+  int insertRecipe(Recipe recipe);
+  // Add all the given ingredients.
+  List<int> insertIngredients(List<Ingredient> ingredients);
+
+  // Deleting unwanted recipes and ingredients:
+
+  // Delete the given recipe.
+  void deleteRecipe(Recipe recipe);
+  // Delete the given ingredient.
+  void deleteIngredient(Ingredient ingredient);
+  // Delete all the given ingredients.
+  void deleteIngredients(List<Ingredient> ingredients);
+  // Delete all the ingredients for the given recipe ID.
+  void deleteRecipeIngredients(int recipeId);
+
+  // Initializing and closing the repository:
+
+  // Allow the repository to initialize. Databases might need to do some startup work.
+  Future init();
+  // Close the repository.
+  void close();
+}
