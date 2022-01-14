ccf038055acf2c9d49d2098ed2f294c65040d616
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-14 08:24:34 +0200

    implementing methods to create and load recipes

--- lib/mock_service/mock_service.dart
+++ lib/mock_service/mock_service.dart
@@ -16,8 +16,36 @@ class MockService {
   // nextRecipe is an instance of Random that creates a number between 0 and 1.
   Random nextRecipe = Random();
 
-  // TODO: Add create and load methods
+  // ----- Implementing methods to create and load recipes:
 
-  // TODO: Add query method
+  // The create() method, which Provider will call, just calls loadRecipes().
+  void create() {
+    loadRecipes();
+  }
 
+  void loadRecipes() async {
+    // rootBundle loads the JSON file as a string.
+    var jsonString = await rootBundle.loadString('assets/recipes1.json');
+    // jsonDecode() creates a map that APIRecipeQuery will use to get a list of recipes.
+    _currentRecipes1 = APIRecipeQuery.fromJson(jsonDecode(jsonString));
+    jsonString = await rootBundle.loadString('assets/recipes2.json');
+    _currentRecipes2 = APIRecipeQuery.fromJson(jsonDecode(jsonString));
+  }
+
+  Future<Response<Result<APIRecipeQuery>>> queryRecipes(
+      String query, int from, int to) {
+    // Use your random field to pick a random integer, either 0 or 1.
+    switch (nextRecipe.nextInt(2)) {
+      case 0:
+        // Wrap APIRecipeQuery result in Success, Response and Future.
+        return Future.value(Response(http.Response('Dummy', 200, request: null),
+            Success<APIRecipeQuery>(_currentRecipes1)));
+      case 1:
+        return Future.value(Response(http.Response('Dummy', 200, request: null),
+            Success<APIRecipeQuery>(_currentRecipes2)));
+      default:
+        return Future.value(Response(http.Response('Dummy', 200, request: null),
+            Success<APIRecipeQuery>(_currentRecipes1)));
+    }
+  }
 }
