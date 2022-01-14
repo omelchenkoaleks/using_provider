d40f5ed0f3fa2baa0cb97f645e01de697add57e0
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-14 08:32:57 +0200

    loading mocked recipes

--- lib/ui/recipes/recipe_list.dart
+++ lib/ui/recipes/recipe_list.dart
@@ -11,6 +11,8 @@ import '../recipe_card.dart';
 import '../recipes/recipe_details.dart';
 import '../colors.dart';
 import '../../data/models/models.dart';
+import '../../mock_service/mock_service.dart';
+import 'package:provider/provider.dart';
 
 class RecipeList extends StatefulWidget {
   const RecipeList({Key? key}) : super(key: key);
@@ -193,7 +195,8 @@ class _RecipeListState extends State<RecipeList> {
       return Container();
     }
     return FutureBuilder<Response<Result<APIRecipeQuery>>>(
-      future: RecipeService.create().queryRecipes(
+      // future: RecipeService.create().queryRecipes(
+      future: Provider.of<MockService>(context).queryRecipes(
           searchTextController.text.trim(),
           currentStartPosition,
           currentEndPosition),
